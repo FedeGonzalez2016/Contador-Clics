d162f4a2f2216c607dd7bb02278c22ce9b5d540f
Federico Gonzalez <federicogonz2016@gmail.com>
2023-02-15 09:38:59 -0300

    Realizada versión 1.0 del contador de Clics

--- src/App.js
+++ src/App.js
@@ -1,47 +1,42 @@
 import "./App.css";
-import Testimonio from "./componentes/Testimonio";
+import Boton from "./componentes/Boton"
+import Contador from "./componentes/Contador";
+import freeCodeCampLogo from "./imagenes/freecodecamp-logo.png"
+import { useState } from "react";
 
 function App() {
+
+  var [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics (numClics + 1);
+  };
+
+  const reiniciarContador = () => {
+    setNumClics (numClics = 0);
+  };
+
   return (
     <div className="App">
-      <div className="contenedor-principal"></div>
-        <h1>Bienvenidos</h1>
-        <h2>Mis Proyectos</h2>
-      <Testimonio
-        proyecto= "'Hotel California'"
-        tecnologias= "Bootstrap,Javascript,Python y Flask"
-        resumen= "Tesis de la Carrera 'Técnico Superior en Desarrollo Web y Aplicaciones digitales' del Instituto Superior Politécnico de Córdoba."
-        imagen = "Hotel-california"
-        link = "https://hcalifornia.innovacionit.tech/"
-      />
-      <Testimonio
-        proyecto= "'Innovación IT'"
-        tecnologias= "Wordpress y Elementor"
-        resumen= "Página Web realizada para simular empresa de tecnología para proyecto académico de la Carrera 'Técnico Superior en Desarrollo Web y Aplicaciones digitales'."
-        imagen = "Innovacion-it"
-        link = "https://innovacionit.tech/"
-      />
-      <Testimonio
-        proyecto= "'Mi Primer Portafolio'"
-        tecnologias= "Html,Css y Javascript"
-        resumen= "Primer Portafolio para exposición de los trabajos realizados."
-        imagen = "Portafolio-1"
-        link = "https://portafoliofg.netlify.app/"
-      />
-      <Testimonio
-        proyecto= "'Encriptador'"
-        tecnologias= "Html,Css y Javascript"
-        resumen= "Página Web Encriptador - Desencriptador de Texto creado como desafío del programa ONE NEXT GENERATION (ALURA LATAM - ORACLE)."
-        imagen = "Encriptador"
-        link = "https://encriptador-desafio.netlify.app/"
-      />
-      <Testimonio
-        proyecto= "'Sistema Medicina Privada'"
-        tecnologias= "Bootstrap,Mysql y PHP"
-        resumen= "Base de datos y programa de una clínica, creado para gestionar información de los pacientes de la misma."
-        imagen = "Crud-medicina-privada"
-        link = "https://github.com/FedeGonzalez2016/CRUD/tree/master"
-      />
+      <div className="freecodecamp-logo-contenedor">
+        <img className="freecodecamp-logo" alt="Logo de freeCodeCamp" src={freeCodeCampLogo}
+        />
+      </div>
+      <div className="contenedor-principal">
+        <Contador
+          numClics= {numClics}
+        />
+        <Boton
+          texto="Clic"
+          esBotonDeClic={true}
+          manejarClic={manejarClic}
+        />
+        <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador}
+        />
+      </div>
     </div>
   );
 }
